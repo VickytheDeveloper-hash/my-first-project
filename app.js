a4b83e3e8873884125b9066059379f582c2c5727
VickytheDeveloper-hash <victorgichuru006@gmail.com>
2025-05-11 16:49:59 +0300

    Add files via upload
    Added the publish server functionality

--- app.js
+++ app.js
@@ -58,3 +58,42 @@ app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 
 app.use('/uploads', express.static('uploads'));
 
+//Adding js to help with dynamic blog publishing
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const BLOG_DATA_PATH = path.join(__dirname, 'data', 'blogs.json');
+
+app.get('/blog', (req, res) => {
+  const blogs = JSON.parse(fs.readFileSync(BLOG_DATA_PATH));
+  res.send(`
+    <h1>Blog Posts</h1>
+    <ul>
+      ${blogs.map(b => `<li><a href="/blog/${b.slug}">${b.title}</a> (${b.date})</li>`).join('')}
+    </ul>
+  `);
+});
+
+app.get('/blog/:slug', (req, res) => {
+  const blogs = JSON.parse(fs.readFileSync(BLOG_DATA_PATH));
+  const blog = blogs.find(b => b.slug === req.params.slug);
+
+  if (!blog) {
+    return res.status(404).send('Blog not found');
+  }
+
+  res.send(`
+    <h1>${blog.title}</h1>
+    <img src="${blog.imageUrl}" width="300" />
+    <div>${blog.content}</div>
+    <p><i>Published on ${blog.date}</i></p>
+    <a href="/blog">← Back to blog</a>
+  `);
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
